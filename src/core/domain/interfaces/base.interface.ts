168c469fd5eb29c41f446d1af6f988e8499251dc
diegohyenna <dgmartins87@hotmail.com>
2023-07-10 15:54:56 -0300

    correção na tipagem

--- src/core/domain/interfaces/base.interface.ts
+++ src/core/domain/interfaces/base.interface.ts
@@ -2,7 +2,7 @@ import { StatusReturn } from "../../infra/http";
 
 export interface IBaseRepository<T> {
   getAll(): Promise<T[]>;
-  getById(id: number): Promise<T | undefined>;
+  getById(id: number): Promise<T>;
   save(obj: T): Promise<StatusReturn>;
   update(id: number, obj: T): Promise<StatusReturn>;
   delete(id: number): Promise<StatusReturn>;
